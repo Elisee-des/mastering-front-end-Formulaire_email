ce3c95549536c6e0300c6d23179e71a543614fec
Elisee-des <esabidani@gmail.com>
2022-04-14 01:02:55 +0000

    envoie d'email avec success.parmetrage des seconde.Verouillage des information jugé sensible

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -7,14 +7,24 @@ const FormTemplate = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    const formMess = document.querySelector("form-message")
+    const formMess = document.querySelector(".form-message")
 
-    emailjs.sendForm('service_4bdqtiz', 'template_hgxq536', form.current, 'KH2PQNfUY8NjGIBJY')
+    emailjs.sendForm(process.env.REACT_APP_SERVICE, process.env.REACT_APP_TEMPLATE, form.current, process.env.REACT_APP_ID)
       .then((result) => {
-          console.log(result.text);
+          console.log(result.text); 
           form.current.reset()
+          formMess.innerHTML = "<p className='success'>Message Envoyé avec succes</p>";
+
+          setTimeout(() => {
+              formMess.innerHTML = "";
+          }, 2500)
       }, (error) => {
           console.log(error.text);
+          formMess.innerHTML = "<p className='error'>Une erreur c'eest produit veuillez reessayé</p>";
+          
+          setTimeout(() => {
+              formMess.innerHTML = "";
+            }, 2500)
       });
   };
 
@@ -22,9 +32,9 @@ const FormTemplate = () => {
       <div className="form-container">
         <form ref={form} onSubmit={sendEmail}>
             <label>Name</label>
-            <input type="text" name="name" required autoComplete='off' />
+            <input type="text" name="name" required />
             <label>Email</label>
-            <input type="email" name="email" required autoComplete='off' />
+            <input type="email" name="email" required />
             <label>Message</label>
             <textarea name="message"  required />
             <input type="submit" value="Send" />
